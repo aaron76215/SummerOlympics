4a2f10658799c94b68cbb53759c039bbe1094c18
aaron76215 <aarongarcia1012@icloud.com>
2026-04-17 13:19:32 -0700

    Initial Commit

---
+++ SummerOlympics/SummerOlympicsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SummerOlympicsApp.swift
+//  SummerOlympics
+//
+//  Created by GARCIA, AARON on 4/17/26.
+//
+
+import SwiftUI
+
+@main
+struct SummerOlympicsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SummerOlympicsTests/SummerOlympicsTests.swift
@@ -0,0 +1,17 @@
+//
+//  SummerOlympicsTests.swift
+//  SummerOlympicsTests
+//
+//  Created by GARCIA, AARON on 4/17/26.
+//
+
+import Testing
+@testable import SummerOlympics
+
+struct SummerOlympicsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
